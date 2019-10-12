d9a7ddce7e5866f35c067e560142fb95b90109ae
dylan326 <dylanoldham326@gmail.com>
2019-10-11 22:46:37 -0400

    created all migrations, troubleshooting errors when migration attempted

--- database/migrations/2019_10_12_015952_create_task_user_table.php
+++ database/migrations/2019_10_12_015952_create_task_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCompaniesTable extends Migration
+class CreateTaskUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,17 @@ class CreateCompaniesTable extends Migration
      */
     public function up()
     {
-        Schema::create('companies', function (Blueprint $table) {
+        Schema::create('task_user', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->longText('description')->nullable();
+            $table->integer('task_id')->unsigned();
             $table->integer('user_id')->unsigned();
+
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('task_id')->references('id')->on('tasks');
             $table->timestamps();
         });
     }
-    
+
     /**
      * Reverse the migrations.
      *
@@ -30,6 +31,6 @@ class CreateCompaniesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('companies');
+        Schema::dropIfExists('task_user');
     }
 }
